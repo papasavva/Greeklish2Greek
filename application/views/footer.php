d09a2a6eef11f516107bd41e5ab4f8c05d3e0aa6
Alexandros Papasavva <papasavva.alexandros@gmail.com>
2015-01-14 21:54:29 +0000

    made textarea bigger and modified footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -6,9 +6,9 @@
 <div id="footer">
 	<div class="container">
 		<p class="text-muted credit text-center">
-			Developed by <a
+			Designed & Developed in UK with <font color="red">♥</font> by <a
 				href="mailto:papasavva.alexandros@gmail.com?Subject=Greeklish2Greek">Alexandros
-				Papasavva</a>
+				Papasavva</a> 
 		</p>
 	</div>
 </div>
--- application/views/home.php
+++ application/views/home.php
@@ -12,7 +12,7 @@ $this->load->view ( 'header' );
 			<div class="col-sm-10 col-md-10">
 				<div class="row text-center">
 					<h3>Write Greeklish here</h3>
-					<textarea id="decodedtext_id" class="form-control" rows="7"></textarea>
+					<textarea id="greeklish_text" class="form-control" rows="14"></textarea>
 				</div>
 			</div>
 			<div class="col-sm-1 col-md-1"></div>
@@ -22,8 +22,8 @@ $this->load->view ( 'header' );
 			<div class="col-sm-1 col-md-1"></div>
 			<div class="col-sm-10 col-md-10">
 				<div class="row text-center">
-					<h3>Write Greeklish here</h3>
-					<textarea id="decodedtext_id" class="form-control" rows="7"></textarea>
+					<h3>Write Greek here</h3>
+					<textarea id="greek_text" class="form-control" rows="14"></textarea>
 				</div>
 			</div>
 			<div class="col-sm-1 col-md-1"></div>
